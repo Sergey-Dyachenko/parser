1f11381e4150f77e76c8f80701a6ddd14907a6c2
sergey <serjdyachenko@gmail.com>
2018-08-09 16:39:07 +0300

    models and nigrations

---
+++ database/migrations/2018_08_09_114128_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('film_name');
+            $table->string('duration');
+            $table->string('director');
+            $table->string('poster_url');
+            $table->text('summary_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
